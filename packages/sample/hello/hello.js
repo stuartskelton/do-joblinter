68c12deb1b8bae8ada59f87bdf13bacfd790c297
Stuart Skelton <stuartskelton@users.noreply.github.com>
2023-01-16 21:34:59 +0000

    return the linter data

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -2,11 +2,9 @@ const joblint = require('joblint')
 
 function main(args) {
     let job = args.name || 'stranger';
-   let linted_data = joblint(job);
+    let linted_data = joblint(job);
     console.log(linted_data);
-    let name =  "hi";
-    let greeting = 'Hello ' + name + '!';
-    return {"body": greeting};
+    return linted_data
   }
 
 exports.main = main
