cdf6378e698350153263a302e677a0eb23bae16c
Stuart Skelton <stuartskelton@users.noreply.github.com>
2023-01-16 21:59:39 +0000

    added a slighlt nicer example

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -1,11 +1,25 @@
-const joblint = require('joblint')
+const { joblint } = require('joblint')
+const { StatusCodes, getReasonPhrase } = require('http-status-codes');
 
 function main(args) {
-  let job = args.name || 'stranger';
-  console.log("123");
-  let linted_data = joblint(job);
-  console.log(linted_data);
-  return { "data" : linted_data }
+  try {
+    let job = args.name || 'stranger';
+    console.log("123");
+    let linted_data = joblint(job);
+    console.log(linted_data);
+    return { "body" : linted_data }
+
+  } catch (error) {
+    console.error(error)
+    return {
+      error: {
+        statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+        body: {
+          message: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR)
+        }
+      }
+    }
+  }
 }
 
 exports.main = main
