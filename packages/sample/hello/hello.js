fa85736984e39262a051f65065adbfcb735c8d22
Stuart Skelton <stuartskelton@users.noreply.github.com>
2023-01-16 21:31:46 +0000

    fixed a parameter

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -1,11 +1,12 @@
 const joblint = require('joblint')
 
 function main(args) {
+    let job = args.name || 'stranger';
    let linted_data = joblint(job);
-    console.log(linted_data)
+    console.log(linted_data);
     let name =  "hi";
-    let greeting = 'Hello ' + name + '!'
-    return {"body": greeting}
+    let greeting = 'Hello ' + name + '!';
+    return {"body": greeting};
   }
 
 exports.main = main
