99c15f08bec1eeadb2c6c883d7bde9371e085956
Stuart Skelton <stuartskelton@users.noreply.github.com>
2023-01-16 22:02:49 +0000

    revert and try body

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -1,25 +1,11 @@
-const { joblint } = require('joblint')
-const { StatusCodes, getReasonPhrase } = require('http-status-codes');
+const joblint = require('joblint')
 
 function main(args) {
-  try {
-    let job = args.name || 'stranger';
-    console.log("123");
-    let linted_data = joblint(job);
-    console.log(linted_data);
-    return { "body" : linted_data }
-
-  } catch (error) {
-    console.error(error)
-    return {
-      error: {
-        statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-        body: {
-          message: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR)
-        }
-      }
-    }
-  }
+  let job = args.name || 'stranger';
+  console.log("1234");
+  let linted_data = joblint(job);
+  console.log(linted_data);
+  return { "body" : linted_data }
 }
 
 exports.main = main
