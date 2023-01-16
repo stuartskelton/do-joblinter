adf5198e7d807c7768ee1ea6fdad337d73ab7e18
Stuart Skelton <stuartskelton@users.noreply.github.com>
2023-01-16 21:48:08 +0000

    explicit json object return

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -1,11 +1,11 @@
 const joblint = require('joblint')
 
 function main(args) {
-    let job = args.name || 'stranger';
-    console.log("123");
-    let linted_data = joblint(job);
-    console.log(linted_data);
-    return linted_data
-  }
+  let job = args.name || 'stranger';
+  console.log("123");
+  let linted_data = joblint(job);
+  console.log(linted_data);
+  return { "data" : linted_data }
+}
 
 exports.main = main
