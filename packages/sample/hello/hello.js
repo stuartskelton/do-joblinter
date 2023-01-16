82e37c0007215c9b554e2954ab530dade807095c
Stuart Skelton <stuartskelton@users.noreply.github.com>
2023-01-16 21:40:54 +0000

    Added a console log so I know its been deployed

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -2,6 +2,7 @@ const joblint = require('joblint')
 
 function main(args) {
     let job = args.name || 'stranger';
+    console.log("123");
     let linted_data = joblint(job);
     console.log(linted_data);
     return linted_data
